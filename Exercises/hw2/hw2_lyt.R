f8cca4b22bdf1fa10890a75f24dedc819b813044
Liang YiTing <799300435@qq.com>
2020-11-03 15:22:53 +0800

    作业2修改

--- Exercises/hw2/hw2_lyt.R
+++ Exercises/hw2/hw2_lyt.R
@@ -18,11 +18,13 @@ e = data.frame(a = 1:4,
 f = list(name = 'Fred', wife = 'Mary', no.children = 3, child.ages = c(4,7,9)) ;f   #列表
 
 
+
 # 2. 列出当前系统中所有自带数据集，查看faithful数据集内容和帮助
 
 data()       # 已载入包的数据集
 str(faithful)       
 help(faithful)
+
 
 
 # 3. 把data文件夹中的 数据.xls 导入到R中
@@ -32,9 +34,11 @@ data = read.csv('./data/数据.csv', header = T)   # header参数表示是否将
 head(data)
 
 
+
 # 4. 显示 数据.xls 数据集的结构
 
 str(data)      # 显示数据的结构：使用str函数
+
 
 
 # 5. 对 数据.xls 数据集进行子集的提取：
@@ -47,6 +51,7 @@ data2 = data[data[,1]>1978,]
 data3 = data[(data[,1]>1978)&(data[,6]>1)&(data[,6]<1.2),]
 
 
+
 # 6. 把data文件夹中的 GDP.xls 中的4个子表都导入到R中，并把其转换成符合面板数据要求的数据（as.matrix,as.vector，rbind）
 
 install.packages("xlsx")
@@ -54,17 +59,25 @@ library(xlsx)
   
 data_list = list()
 for(i in 1:4){
-    data_list[[i]] = read.xlsx('./data/GDP.xls', i, encoding = 'UTF-8')}
+    data_list[[i]] = read.xlsx('./data/GDP.xls', i, encoding = 'UTF-8')}     # 保证每一个sheet的年份取值均为1949-2011年, 需补充缺失行
 
-m1 = as.matrix(data_list[[1]])   # 第一列为年份
-m2 = as.matrix(data_list[[2]])   # 第一列为北京
-m3 = as.matrix(data_list[[3]][,-34])   # 第一列为年份
-m4 = as.matrix(data_list[[4]])   # 第一列为北京
+# 数据先转换为矩阵，再转化为向量
+m1 = as.matrix(data_list[[1]])   
+v1 = as.vector(m1[,-1])    # 删除第一列(年份),将数据转化为按列排序的向量
+m2 = as.matrix(data_list[[2]])   
+v2 = as.vector(m2[,-1])    
+m3 = as.matrix(data_list[[3]][,-34])   # 最后一列均为NA，删去   
+v3 = as.vector(m3[,-1])    
+m4 = as.matrix(data_list[[4]])   
+v4 = as.vector(m4[,-1])    
 
-gdp = rbind(m1, m2, m3, m4)
+year = m1[,1]     # 提取年份取值
+province = colnames(m1[,-1])  # 提取除年份外的列名(省份)
+year_province = expand.grid(year, province)
 
-
-#? 什么叫符合面板数据要求
+combine = cbind(year_province, v1, v2, v3, v4)
+colnames(combine)<-c("年份","省份","GDP","GDP指数","人均GDP","人均GDP指数")
+View(combine)
 
 
 
@@ -76,6 +89,7 @@ gdp = rbind(m1, m2, m3, m4)
 write.csv(faithful, file = "./faithful.csv", row.names = FALSE)   # 不保留表格列序行号
 write.csv(data3, file = './data3.csv', row.names = FALSE)
 write.csv(gdp, file = './面板数据.csv', row.names = FALSE)
+write.csv(combine, file = './gdp_combine.csv', row.names = FALSE)
 
 
 
