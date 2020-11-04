003ee2b7651820760bce1003c2f4d9627f748e70
Liao Mingke <951368155@qq.com>
2020-11-04 16:39:42 +0800

    hw2 update

--- Exercises/hw2/hw2_lmk.R
+++ Exercises/hw2/hw2_lmk.R
@@ -35,26 +35,36 @@ data <- na.omit(data)
 data[data[,1]>=1978,]
 
 ##  3.提取1978年之后且 KR 变量在 1~1.2之间的数据
-data[data$Year>=1978 & data$KR>=1 & data$KR<=1.2,]
+data3 <- data[data$Year>=1978 & data$KR>=1 & data$KR<=1.2,]
 
-# 6.把data文件夹中的 GDP.xls 中的4个子表都导入到R中，并把其转换成符合面板数据要求的数据（as.matrix,as.vector,rbind）
+# 6.把data文件夹中的 GDP.xls 中的4个子表都导入到R中，并把其转换成符合面板数据要求的数据（as.matrix,as.vector,rbind,expand.grid()
 data.list <- list()
 for (i in 1:4){
     data.list[[i]]=read.xlsx('D:/zuel_master/R/R-Learning/Exercises/data/GDP.xls',i,encoding='UTF-8')
 }
-a <- as.matrix(data.list[[1]])
-b <- as.matrix(data.list[[2]])
-c <- as.matrix(data.list[[3]][,-34])
-d <- as.matrix(data.list[[4]])
-gdp <- rbind(a,b,c,d)
+a1 <- as.matrix(data.list[[1]])
+a2 <- as.vector(a1[,-1])
+b1 <- as.matrix(data.list[[2]])
+b2 <- as.vector(b1[,-1])
+c1 <- as.matrix(data.list[[3]][,-34]) # 第34列为多余的缺失值列
+c2 <- as.vector(c1[,-1])
+d1 <- as.matrix(data.list[[4]])
+d2 <- as.vector(d1[,-1])
+
+year <- a1[,1] # 年份列
+dist <- colnames(a1[,-1]) # 地区列
+rowindex <- expand.grid(year,dist) # 构造年份+地区的行索引 
+gdp <- cbind(rowindex,a2,b2,c2,d2) # 需保证行数相同，年份为1949-2011
+colnames(gdp) <- c('年份','地区','GDP','GDP指数','人均GDP','人均GDP指数') # 增加列名
+View(gdp)
 
 # 7.保存数据
 ##  1.写出faithful数据集
 data(faithful)
-write.table(faithful,'D:/zuel_master/R/R-Learning/Exercises/hw2/faithful.txt')
+write.csv(faithful,'D:/zuel_master/R/R-Learning/Exercises/hw2/faithful.csv')
 
 ##  2.写出第5题中最后提取的子集
-write.table(data,'D:/zuel_master/R/R-Learning/Exercises/hw2/data.txt')
+write.csv(data3,'D:/zuel_master/R/R-Learning/Exercises/hw2/data.csv')
 
 ##  3.写出导入 GDP.xls 并整理好之后的数据集
-write.table(gdp,'D:/zuel_master/R/R-Learning/Exercises/hw2/gdp.txt')
+write.csv(gdp,'D:/zuel_master/R/R-Learning/Exercises/hw2/gdp.csv')
